76aa94fda7927a1d443314365811a231336f6704
Sebastian Guerrero <sguerrer@andrew.cmu.edu>
2016-04-25 10:01:07 -0400

    added the sessions and user controller, also added the routes to the config file

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # authorize_resource
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+  end
+
+  def create
+    
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+    def set_user
+      
+    end
+
+    def user_params
+      
+    end
+end
