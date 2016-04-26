2d9f6d7816f0641fb0638852dae8c901b840500d
Sebastian Guerrero <sguerrer@andrew.cmu.edu>
2016-04-26 00:43:44 -0400

    uncomented the code for session s controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,19 @@ class SessionsController < ApplicationController
     end
 
     def create
-      # user = User.find_by_email(params[:email])
-      # if user && User.authenticate(params[:email], params[:password])
-      #   session[:user_id] = user.id
-      #   redirect_to home_path, notice: "Logged in!"
-      # else
-      #   flash.now.alert = "Email or password is invalid"
-      #   render "new"
-      # end
+      user = User.find_by_email(params[:email])
+      if user && User.authenticate(params[:email], params[:password])
+        session[:user_id] = user.id
+        redirect_to home_path, notice: "Logged in!"
+      else
+        flash.now.alert = "Email or password is invalid"
+        render "new"
+      end
     end
 
     def destroy
-      # session[:user_id] = nil
-      # redirect_to home_path, notice: "Logged out!"
+      session[:user_id] = nil
+      redirect_to home_path, notice: "Logged out!"
     end
 
 end
