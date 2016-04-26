fb8321d2eacd845d9337591fae636e83b2f3e441
Sebastian Guerrero <sguerrer@andrew.cmu.edu>
2016-04-25 23:34:37 -0400

    added authificaiton method to users and list, also added some code to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,12 @@ class UsersController < ApplicationController
   # authorize_resource
 
   def index
-
   end
 
   def show
-
   end
 
   def new
-
   end
 
   def edit
@@ -22,16 +19,16 @@ class UsersController < ApplicationController
   end
 
   def update
-
+    
   end
 
   def destroy
-
+    
   end
 
   private
     def set_user
-      
+      @user = User.find(params[:id])
     end
 
     def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   # get modules to help with some functionality
   include CreameryHelpers::Validations
 
+
+  # Use built-in rails support for password protection
+  has_secure_password
+
   # Relationships
   belongs_to :employee
 
@@ -10,6 +14,19 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, :message => "is not a valid format"
   validate :employee_is_active_in_system
   
+
+  # for use in authorizing with CanCan
+  ROLES = [['Administrator', :admin],['Manager', :manager],['Employee', :employee],['Guest', :guest]]
+
+  def self.authenticate(email,password)
+    find_by_email(email).try(:authenticate, password)
+  end
+
+  def role?(authorized_role)
+    return false if role.nil?
+    role.to_sym == authorized_role
+  end
+
   private
   def employee_is_active_in_system
     is_active_in_system(:employee)
