abf70966fe58f1461d77a1bd4a094291047fc640
Sebastian Guerrero <sguerrer@andrew.cmu.edu>
2016-04-27 21:59:07 -0400

    added the pictures

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,41 @@
 //= require semantic_ui/semantic_ui
 //= require_tree .
 
+$('div.itemtags a').addClass('item');
 
+
+// javascripts calls that edit the paginate tags in order to add arrows for previous and next
+$('.previous_page').addClass('icon');
+$('.next_page').addClass('icon');
+$('.previous_page').html('<i class="left chevron icon"></i>');
+$('.next_page').html('<i class="right chevron icon"></i>');
+
+// removes the previous_page and next_page from the a tags
+$( "a" ).removeClass( "previous_page" );
+$( "a" ).removeClass( "next_page" );
+
+// adds a 'ui form' class to all the forms
+$('form').addClass('ui form');
+
+// for all selects tags adds a 'ui fluid search dropdown' class
+$('select').addClass('ui fluid search dropdown');
+
+// triggers dropdowns to work
+$('select.dropdown')
+  .dropdown()
+;
+
+// triggers the checkbox 
+$('.ui.checkbox')
+  .checkbox()
+;
+
+// when you click on the x of the flash messages it delets them with a fade
+$('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
