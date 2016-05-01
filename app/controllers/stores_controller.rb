408e67a3f010a237cfe5e519d1a9afd14977c5c1
Sebastian Guerrero <sguerrer@andrew.cmu.edu>
2016-05-01 12:28:29 -0400

    more details to tables and added gflavors to store page

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -10,6 +10,7 @@ class StoresController < ApplicationController
 
   def show
     @current_assignments = @store.assignments.current.by_employee.paginate(page: params[:page]).per_page(8)
+    @flavors = @store.flavors
   end
 
   def new
